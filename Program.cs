5e3a1f6243343013c882f9739633f07f1f38edc4
Elazar Rosenthal <elazar.rosenthal@idt.net>
2011-09-21 15:01:07 -0400

    new

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestDll;
+
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Value of UselessStuff.myTrue() is " + UselessStuff.myTrue().ToString());
+            // Console.Write("End> "); Console.ReadLine(); // keep from quitting when debugging 
+        }
+    }
+}
